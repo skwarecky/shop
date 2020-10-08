ddc5617218956c090d4fae0df91d3926caae5360
skwarecky <dawid.smalc@gmail.com>
2020-10-08 19:53:16 +0200

    Add product fillables

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -4,8 +4,18 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class Product extends Model
 {
-    use HasFactory;
+    use HasFactory, Notifiable;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'brand', 'name', 'description',  'image', 'categoryid','quantity','price', 'discount'
+    ];
 }
